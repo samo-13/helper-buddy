3dd663cdc41ea6bdd12c21a6c6571191c55ddab3
Megan Therion <meganhutt@hotmail.com>
2022-09-27 15:05:33 -0700

    write migrations for db

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,7 +1,37 @@
 -- Drop and recreate Users table (Example)
 
 DROP TABLE IF EXISTS users CASCADE;
+
+
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE tasks (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  title VARCHAR(255) NOT NULL,
+  started_at TIMESTAMP,
+  completed_at TIMESTAMP
+);
+
+CREATE TABLE steps (
+  id SERIAL PRIMARY KEY NOT NULL,
+  task_id INTEGER REFERENCES tasks(id) ON DELETE CASCADE,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  title VARCHAR(255) NOT NULL,
+  started_at TIMESTAMP,
+  completed_at TIMESTAMP
+);
+
+CREATE TABLE notifications (
+  id SERIAL PRIMARY KEY NOT NULL,
+  task_id INTEGER REFRENCES tasks(id) ON DELETE CASCADE,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  message VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP
+);
+
+
