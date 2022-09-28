2dc9021c31a01bd9a9e5b9734efa8f55d615aa28
Megan Therion <meganhutt@hotmail.com>
2022-09-27 17:09:53 -0700

    seed database

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -9,7 +9,8 @@ DROP TABLE IF EXISTS notifications CASCADE;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE tasks (
@@ -25,7 +26,7 @@ CREATE TABLE steps (
   task_id INTEGER REFERENCES tasks(id) ON DELETE CASCADE,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   title VARCHAR(255) NOT NULL,
-  message text,
+  description VARCHAR(255),
   started_at TIMESTAMP,
   completed_at TIMESTAMP
 );
