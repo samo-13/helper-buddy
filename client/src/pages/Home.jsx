1cbac979da598eb564a1f4ec839eb142d769c9dd
samo-13 <sarah-moss@live.com>
2022-10-01 13:56:18 -0400

    Render task names on home view

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,17 +1,32 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
 
 function Home() {
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    Promise.all([
+      axios.get('http://localhost:8080/api/tasks')
+    ])
+    .then((response) => {
+      const tasks = response[0].data
+      setTasks(tasks)
+    })
+  }, [])
+
   return (
       <div className="App">
         <header className="App-header">
            <h2>Helper Buddy!</h2>
-           <h3>Tasks go here...</h3>
+           {/* <h3>Tasks go here...</h3> */}
+           {tasks.map(task => (<h3>{task.name}</h3>))}
         </header>
         <div>
           <form action="../../post" method="post">
             <button type="submit">Connected?</button>
           </form>
         </div>
+
       </div>
     );
   }
