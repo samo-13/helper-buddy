8436c974e1aa87bc4176ebfbb5bf041359843426
samo-13 <sarah-moss@live.com>
2022-10-01 15:32:27 -0400

    Add box style

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from "react";
 import axios from 'axios';
+import './freshTaskItem.scss';
 
 function Home() {
   const [tasks, setTasks] = useState([])
@@ -18,8 +19,11 @@ function Home() {
       <div className="App">
         <header className="App-header">
            <h2>Helper Buddy!</h2>
-           {/* <h3>Tasks go here...</h3> */}
-           {tasks.map(task => (<h3>{task.name}</h3>))}
+
+           {tasks.map(task => (
+           <h3 class='freshTask'>{task.name}</h3>
+
+           ))}
         </header>
         <div>
           <form action="../../post" method="post">
