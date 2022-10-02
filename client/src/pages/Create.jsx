d68503ba41db85b39f56b9822a7a14a9dbe759a4
samo-13 <sarah-moss@live.com>
2022-10-02 13:22:35 -0400

    Begin basic form for create task

--- client/src/pages/Create.jsx
+++ client/src/pages/Create.jsx
@@ -1,9 +1,21 @@
+import './Create.scss';
+import '../components/Button.scss';
+
 const Create = () => {
   return (
-    <div>
-      <h1>Create</h1>
+    <div className="wrapper">
+      <h1>Create your own custom task!</h1>
+      <form>
+      <fieldset>
+         <label>
+           <p>Task Name</p>
+           <input name="task" />
+         </label>
+       </fieldset>
+       <button class="button" type="submit">Create</button>
+      </form>
     </div>
-  );
-};
+  )
+}
 
 export default Create;
