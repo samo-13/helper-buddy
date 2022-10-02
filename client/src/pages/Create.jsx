cc59ab1fff1fef5e665776222ac8eea982068c24
samo-13 <sarah-moss@live.com>
2022-10-02 14:49:27 -0400

    Add box shadow to task cards

--- client/src/pages/Create.jsx
+++ client/src/pages/Create.jsx
@@ -2,17 +2,22 @@ import './Create.scss';
 import '../components/Button.scss';
 
 const Create = () => {
+  const handleSubmit = event => {
+    event.preventDefault();
+    alert('You have created a new task.')
+  }
+
   return (
     <div className="wrapper">
       <h1>Create your own custom task!</h1>
-      <form>
-      <fieldset>
-         <label>
-           <p>Task Name</p>
-           <input name="task" />
-         </label>
-       </fieldset>
-       <button class="button" type="submit">Create</button>
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <label>
+            <p>Task Name</p>
+            <input name="name" />
+          </label>
+        </fieldset>
+        <button class='button' type="submit">Create</button>
       </form>
     </div>
   )
