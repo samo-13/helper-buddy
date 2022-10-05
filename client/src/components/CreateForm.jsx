e565bdbf4cb34d6ce64bb849e85f35dada43612f
samo-13 <sarah-moss@live.com>
2022-10-05 15:32:42 -0400

    Get task name rendering state in form component

--- client/src/components/CreateForm.jsx
+++ client/src/components/CreateForm.jsx
@@ -6,7 +6,7 @@ import React, { useState, useEffect } from 'react';
 class Form extends React.Component {
 
   state = {
-    steps: [{name: "", stepDescrption: ""}] // put first blank step object into our state
+    steps: [{name: "", descrption: ""}] // put first blank step object into our state
   }
 
   addStep = (event) => { // sets the state with ... of the previous state’s steps array with a new blank step object added on the end. This should re-render our component, and the form will iterate over the new length of the array, to give us a new pair of inputs.
@@ -15,20 +15,33 @@ class Form extends React.Component {
     }));
   }
 
-  removeStep = () => {
-    alert('Remove Step!!')
-    };
+  removeStep(index) {
+    let steps = this.state.steps;
+    steps.splice(index, 1);
+    this.setState({ steps });
+  }
 
   handleSubmit = (event) => { // stops form from automatically reloading the page
     event.preventDefault()
   }
 
+  handleChange = (event) => { // Hhndling our dynamic inputs
+    if (["name", "description"].includes(event.target.className)) { // check if the event’s class matches the dynamic inputs
+      let steps = [...this.state.steps] // if sos, make a copy of our steps array of objects using the spread operator
+      steps[event.target.database.id][event.target.className] = event.target.value // uses event.target’s dataset to match the input to its corresponding object -- then use the event.target’s classname to grab either the step object’s name or description value -- e.g., steps[0][name] = typed text
+      this.setState({ steps }, () => console.log(this.state.steps)) // with our copied list mutated to reflect the new input, we use setState to save the change to our state and trigger a re-render of our form.
+    } else {
+      // event.target.value grabs the input value that the user types into
+      this.setState({ [event.target.name]: event.target.value }) // using [] to dynamically match our state using each input’s name attribute (Computed Property Names)
+    } // https://medium.com/@zacjones/handle-multiple-inputs-in-react-with-es6-computed-property-name-e3d41861ae46
+  } // e.g., our taskName input has a name of task, which means our setState translates to task: whatever-was-typed
+
   render() {
-    let {steps} = this.state
+    let { task, steps } = this.state
     console.log('THIS STATE:', this.state)
 
     return (
-      <form onSubmit={this.handleSubmit}>
+      <form onSubmit={this.handleSubmit} onChange={this.handleChange}>
         <label>Task Name</label>
         <input type="text" name="taskName" id="taskName" />
         {
@@ -62,7 +75,7 @@ class Form extends React.Component {
         }
         <br></br>
         <button onClick={this.addStep} className="step-button">+ Step</button>
-        <button onClick={this.removeStep} className="step-button">- Step</button>
+        {/* <button onClick={() => this.removeStep(index)} className="step-button">- Step</button> */}
         <input type="submit" value="Submit" />
       </form>
     )
