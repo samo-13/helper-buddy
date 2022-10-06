6c2465ed7d610ed08b9f1313ff91814fa0f6c2d9
Megan Therion <meganhutt@hotmail.com>
2022-10-05 18:00:15 -0700

    switch to task templates JSON

--- client/src/pages/Task.jsx
+++ client/src/pages/Task.jsx
@@ -3,25 +3,28 @@ import { motion, useCycle, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import StepList from '../components/StepList';
 
+
 const Task = () => {
   const { id } = useParams();
   const [task, setTask] = useState(null);
 
   useEffect(() => {
     axios
-      .get(`/api/tasks/${id}`)
+      .get(`/api/tasks/templates/${id}`)
       .then(res => {
         const task = res.data;
 
-        setTask(task);
+        console.log(task[0].task.id);
+        const filteredTasks = task.filter(t => t.task.id == id)
+      
+        setTask(filteredTasks[0])
       })
       .catch(err => {
         console.log(err);
       });
   }, [id]);
 
-
-  return <div>{task && <StepItem {...task} />}</div>;
+  return <div>{task && <StepList {...task} />}</div>;
 };
 
 export default Task;
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const taskTemplate = require("../config/taskInfo.json");
 const {
   getAllTasks,
   createTask,
@@ -11,8 +12,9 @@ const {
 const router = express.Router();
 
 router.route("/").get(getAllTasks).post(createTask);
+router.route("/templates/:id").get((req, res) => res.json(taskTemplate))
+router.route("/:id").get(getTask).delete(deleteTask);
 
-router.route("/:id").get(getTask).put(updateTask).delete(deleteTask);
 
 // router.route("/:id/steps").get(getAllStepsByTaskId).post(createTask);
 
