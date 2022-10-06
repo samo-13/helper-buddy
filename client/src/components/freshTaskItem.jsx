132be58b6da23c4349b941babe147315aba7a2c0
Megan Therion <meganhutt@hotmail.com>
2022-10-06 10:11:05 -0700

    move link to Start button

--- client/src/components/freshTaskItem.jsx
+++ client/src/components/freshTaskItem.jsx
@@ -1,15 +1,39 @@
+import axios from "axios";
 import { Link } from "react-router-dom";
 import "./Taskbox.scss";
+
+
+const newTaskFromClick = document.querySelectorAll('start-task');
+console.log("newtaskfromclicK", newTaskFromClick)
+
+newTaskFromClick.forEach(task => {
+  task.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const taskData = new taskData(e.target);
+  })
+  // axios
+  //     .post(e.target.action)
+  //     .then(res => {
+  //       console.log(res)
+  //     })
+  //     .catch(console.error);
+})
 
 const FreshTaskItem = ({ task }) => {
   return (
     // add onClick = {action} here
     <div className="taskbox" > 
+    {/* here's where the submit button goes */}
+    {/* also add preview button with link to `task/${task.id}` */}
+      {/* make this link just the text */}
+      <h3>{task.name}</h3>
       <Link to={`/task/${task.id}`}>
-        <span>-- Steps</span>
-        <h3>{task.name}</h3>
-        <h4>Average time to complete:</h4>
+      <button 
+      type="submit"
+        class="start-task">Start</button>
+       
       </Link>
+      <h4>Average time to complete:</h4>
     </div>
   );
 };
