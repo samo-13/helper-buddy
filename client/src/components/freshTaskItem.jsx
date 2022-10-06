127a7063a94330bc7d44d6486319cca25291ce44
Megan Therion <meganhutt@hotmail.com>
2022-10-06 10:15:34 -0700

    add preview button to same link

--- client/src/components/freshTaskItem.jsx
+++ client/src/components/freshTaskItem.jsx
@@ -2,36 +2,38 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 import "./Taskbox.scss";
 
+const newTaskFromClick = document.querySelectorAll("start-task");
+console.log("newtaskfromclicK", newTaskFromClick);
 
-const newTaskFromClick = document.querySelectorAll('start-task');
-console.log("newtaskfromclicK", newTaskFromClick)
-
-newTaskFromClick.forEach(task => {
-  task.addEventListener('submit', (e) => {
-    e.preventDefault()
+newTaskFromClick.forEach((task) => {
+  task.addEventListener("submit", (e) => {
+    e.preventDefault();
     const taskData = new taskData(e.target);
-  })
+  });
   // axios
   //     .post(e.target.action)
   //     .then(res => {
   //       console.log(res)
   //     })
   //     .catch(console.error);
-})
+});
 
 const FreshTaskItem = ({ task }) => {
   return (
     // add onClick = {action} here
-    <div className="taskbox" > 
-    {/* here's where the submit button goes */}
-    {/* also add preview button with link to `task/${task.id}` */}
+    <div className="taskbox">
+      {/* here's where the submit button goes */}
+      {/* also add preview button with link to `task/${task.id}` */}
       {/* make this link just the text */}
       <h3>{task.name}</h3>
       <Link to={`/task/${task.id}`}>
-      <button 
-      type="submit"
-        class="start-task">Start</button>
-       
+        <button type="submit" class="start-task">
+          start
+        </button>
+        <button type="click" class="preview-task">
+          preview
+        </button>
+
       </Link>
       <h4>Average time to complete:</h4>
     </div>
