12f2f3f02b03eb13485af9ffc6886ed4203c44da
samo-13 <sarah-moss@live.com>
2022-10-07 20:53:16 -0400

    Add psuedo code + progressBar file setup

--- client/src/components/ProgressBar.jsx
+++ client/src/components/ProgressBar.jsx
@@ -4,6 +4,15 @@
 // Stylesheets
 import './ProgressBar.scss';
 // --------------------------------------------------------------------------------------------------
+// PSEUDO CODE
+// --- Shows the user a visual representation of their progress using a colored bar based on their task step completion status
+// --- Also shows the user the % of completion
+// --- Props that change the height/width/background color
+// --------------------------------------------------------------------------------------------------
 
+const ProgressBar = (props) => {
+  console.log('I AM A PROGRESS BAR!')
+}
 
-
+// --------------------------------------------------------------------------------------------------
+export default ProgressBar;
