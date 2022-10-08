feb879b2ee22a293de51e64887de4f3858b7668e
Billy Wong <bwong.yh@protonmail.com>
2022-10-08 16:25:24 +0900

    create sendText function

--- bin/sendNotification.js
+++ bin/sendNotification.js
@@ -3,3 +3,15 @@ const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken, {
   lazyLoading: true,
 });
+
+const sendText = (message, userPhoneNumber) => {
+  client.messages
+    .create({
+      body: message,
+      from: '+16402033247',
+      to: userPhoneNumber,
+    })
+    .then(message => console.log(message.sid));
+};
+
+module.exports = sendText;
