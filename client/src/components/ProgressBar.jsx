0833ca73a3d082c97cdf0a19b7915f750c54253c
samo-13 <sarah-moss@live.com>
2022-10-07 21:05:43 -0400

    Complete pseudo code

--- client/src/components/ProgressBar.jsx
+++ client/src/components/ProgressBar.jsx
@@ -8,6 +8,12 @@ import './ProgressBar.scss';
 // --- Shows the user a visual representation of their progress using a colored bar based on their task step completion status
 // --- Also shows the user the % of completion
 // --- Props that change the height/width/background color
+
+// -- There is a:
+// --- parent div to rep the whole progress bar
+// --- child div to show the completed part
+// --- span to show completed %
+
 // --------------------------------------------------------------------------------------------------
 
 const ProgressBar = (props) => {
